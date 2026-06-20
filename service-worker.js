837cac4189a1e9f7f71eb727f5b2633dd98ae70f
toolstree <232294003+toolstree@users.noreply.github.com>
2026-06-20 19:52:57 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,28 @@
+const CACHE_NAME = "imignarx-v1";
 
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/style.css",
+  "/js/app.js",
+  "/js/questions.js",
+  "/js/scoring.js",
+  "/js/archetypes.js",
+  "/js/email.js",
+  "/js/analytics.js",
+  "/data/funnels.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
