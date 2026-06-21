80ac87e8cb20d5b862ad2f9c04108f3ac1276668
toolstree <232294003+toolstree@users.noreply.github.com>
2026-06-21 09:09:22 +0530

    Update email.js

--- js/email.js
+++ js/email.js
@@ -1,2 +1,2 @@
 const APPS_SCRIPT_URL =
-"https://script.google.com/macros/s/AKfycbzzSBU6XKEqapGvD2QbZSWpL33kQRllskxR6zXaElxzndm5SfegNA5Ekr1CLWlpsDoe/exec";
+"https://script.google.com/macros/s/AKfycbxs04VvOTxRFtyt8Otr9TigGEZAsGKaLccmt-JpsYG428fJ6fDm432NIVtmD31b5zCS/exec";
